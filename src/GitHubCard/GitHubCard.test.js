96b61c54395952e0b48579458b2f337eeb40a634
danazou <dana.sou@gmail.com>
2023-01-04 23:19:47 -0800

    snapshot test for card passed

--- src/GitHubCard/GitHubCard.test.js
+++ src/GitHubCard/GitHubCard.test.js
@@ -1,22 +1,7 @@
-import React from "react"
-import Card from "react-bootstrap/Card"
+import GitHubCard from './GitHubCard'
+import renderer from 'react-test-renderer'
 
-
-function GitHubCard() {
-  return (
-      <div className="card">
-        <Card style={{ width: "25rem"}}>
-        <Card.Img variant="top" src="https://avatars.githubusercontent.com/u/106777410?s=96&v=4" />
-        <Card.Body>
-            <Card.Title>Dana</Card.Title>
-            <Card.Text>
-            Hi - my name is Dana. I am currently studying software development. 
-            it has but extremely challenging but also fun.
-            </Card.Text>
-        </Card.Body>
-        </Card>
-      </div>
-  );
-}
-
-export default GitHubCard
+test('renders a snapshot', () => {
+  const tree = renderer.create(<GitHubCard />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
